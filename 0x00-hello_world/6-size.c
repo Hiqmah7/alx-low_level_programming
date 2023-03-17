746c41abeadeb2b0f38c3288a1ed6466b489c287
Hiqmah7 <auduabdulhakeem@gmail.com>
2023-03-17 16:00:01 -0700

    100-intel commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print size of data types
+ *
+ * Return:0(sucessful)
+ */
+
+int main(void)
+{
+  char a;
+  int b;
+  long int c;
+  long long int d;
+  float x;
+
+  printf("Size of a char: %lu byte(s)\n", (unsigned long)(sizeof(a))
+         printf(
