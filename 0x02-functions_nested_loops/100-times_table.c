e74c0d203350624946357847cabe39040baac0ea
Hiqmah7 <auduabdulhakeem@gmail.com>
2023-03-23 03:34:41 -0700

    Awesome tasks

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,78 +1,25 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Description: computes and prints the sum of all the multiples of 3 or
+ * 5 below 1024 (excluded), followed by a new line
+ * Return: 0
+ */
+int main(void)
+{
+	int c = 0;
+	int sum = 0;
 
-
-         /**
-	 * print_times_table - prints the n times table, starting with 0
-	 * @n: number of the times table
-	 */
-	void print_times_table(int n)
+	while (c < 1024)
 	{
-		int i, j, k;
-
-
-		if (n >= 0 && n <= 15)
+		if (c % 3 == 0 || c % 5 == 0)
 		{
-			for (i = 0; i <= n; i++)
-			{
-				for (j = 0; j <= n; j++)
-				{
-					k = j * i;
-					if (j == 0)
-					{
-						_putchar(k + '0');
-					} else if (k < 10 && j != 0)
-					{
-						_putchar(',');
-						_putchar(' ');
-						_putchar(' ');
-						_putchar(' ');
-						_putchar(k + '0');
-					} else if (k >= 10 && k < 100)
-					{
-						_putchar(',');
-						_putchar(' ');
-						_putchar(' ');
-						_putchar((k / 10) + '0');
-						_putchar((k % 10) + '0');
-					} else if (k >= 100)
-					{
-						_putchar(',');
-						_putchar(' ');
-						_putchar((k / 100) + '0');
-						_putchar(((k / 10) % 10) + '0');
-						_putchar((k % 10) + '0');
-					}
-				}
-				_putchar('\n');
-			}
+			sum += c;
 		}
+
+		c++;
 	}
-
-
-
-
-=======================================
-
-
-100-main.c
-
-
-#include "main.h"
-
-
-	/**
-	 * main - check the code.
-	 *
-	 * Return: Always 0.
-	 */
-	int main(void)
-	{
-	    print_times_table(3);
-	    _putchar('\n');
-	    print_times_table(5);
-	    _putchar('\n');
-	    print_times_table(98);
-	    _putchar('\n');
-	    print_times_table(12);  
-	    return (0);
-	}
+	printf("%i\n", sum);
+	return (0);
+}
