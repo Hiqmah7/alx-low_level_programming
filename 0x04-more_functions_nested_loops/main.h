54e69cbad9311b879364da04601b677d2826e57f
Hiqmah7 <auduabdulhakeem@gmail.com>
2023-03-23 14:31:01 -0700

    Enjoying working with c

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+Int_putchar(char c);
+Int_isupper(int c);
+Int_isdigit(int c);
+Int mul(int a, int b);
+Void print_numbers(void);
+Void print_most_numbers(void);
+Void more_numbers(void);
+Void print_line(int n);
+Void print_diagonal(int n);
+Void print_square(int size);
+Void print_traiangle(int size);
+Void print_number(int n);
+#endif /* MAIN_H */
