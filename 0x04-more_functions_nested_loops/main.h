a416da5f5e7dad0b2049fcf567aa6b6ffaeca55f
Hiqmah7 <auduabdulhakeem@gmail.com>
2023-03-24 07:17:27 -0700

    0-isupper.c commit

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,15 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
-Int_putchar(char c);
-Int_isupper(int c);
-Int_isdigit(int c);
-Int mul(int a, int b);
-Void print_numbers(void);
-Void print_most_numbers(void);
-Void more_numbers(void);
-Void print_line(int n);
-Void print_diagonal(int n);
-Void print_square(int size);
-Void print_traiangle(int size);
-Void print_number(int n);
-#endif /* MAIN_H */
+
+int _putchar(int c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+
+#endif
