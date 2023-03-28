2dc5e64419431fd5b96deceb739b217114a3df0e
Hiqmah7 <auduabdulhakeem@gmail.com>
2023-03-28 12:49:23 -0700

    pointers is fun

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,18 +1,24 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include "main.h"
 
 /**
- * print_rev - Prints a string in reverse.
- * @s: string to be reserved.
- */
+* print_rev - Write a function that prints a string, in reverse,
+* followed by a new line.
+*
+* @s: character to check
+*
+* Return: (void)
+*/
 void print_rev(char *s)
 {
-int len = 0, i;
-
-while (s[i++])
-len++;
-
-for (i = len - 1; i >= 0; i--)
+int length = strlen(s);
+int i;
+for (i = length - 1 ; i >= 0; i--)
+{
 _putchar(s[i]);
-
+}
 _putchar('\n');
 }
