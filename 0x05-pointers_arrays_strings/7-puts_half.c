29ff2f1ae108ae9675ecb9a96e97d3fe166023bf
Hiqmah7 <auduabdulhakeem@gmail.com>
2023-03-28 12:59:26 -0700

    pointers is fun

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,25 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
 #include "main.h"
+
 /**
- * puts_half - a function that prints half of a string
- * if odd len, n = (length_of_the_string - 1) / 2
- * @str: input
- * Return: half of input
- */
+* puts_half - Write a function that prints half of a string,
+* followed by a new line.
+*
+* @str: character to check
+*
+* Return: (void)
+*/
 void puts_half(char *str)
 {
-int a, n, longi;
-
-longi = 0;
-
-for (a = 0; str[a] != '\0'; a++)
-longi++;
-
-n = (longi / 2);
-
-if ((longi % 2) == 1)
-n = ((longi + 1) / 2);
-
-for (a = n; str[a] != '\0'; a++)
-_putchar(str[a]);
+int length = strlen(str);
+int i;
+if (length % 2 == 0)
+{
+for (i = length / 2; i < length ; i++)
+{
+_putchar(str[i]);
+}
+}
+else if  (length % 2 != 0)
+{
+for (i = (length + 1) / 2; i < length; i++)
+{
+_putchar(str[i]);
+}
+}
 _putchar('\n');
 }
