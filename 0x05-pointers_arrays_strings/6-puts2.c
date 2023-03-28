b77455e11093838a139af2c64e424ed9713b8a90
Hiqmah7 <auduabdulhakeem@gmail.com>
2023-03-28 12:52:05 -0700

    pointers is fun

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,18 +1,24 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include "main.h"
 
 /**
- * puts2 - Prints one char out of two of a string.
- * @str: The string containing characters.
- */
+* puts2 - Write a function that prints every other character of a string,
+* starting with the first character, followed by a new line.
+*
+* @str: character to check
+*
+* Return: (void)
+*/
 void puts2(char *str)
 {
-int i, len = 0;
-
-while (str[i++])
-len++;
-
-for (i = 0; i < len; i += 2)
+int length = strlen(str);
+int i;
+for (i = 0 ; i < length; i += 2)
+{
 _putchar(str[i]);
-
+}
 _putchar('\n');
 }
