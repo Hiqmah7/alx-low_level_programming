0515125c110f1b17c7317c0b3a26c4a3d0bd6cb4
Hiqmah7 <auduabdulhakeem@gmail.com>
2023-04-05 07:20:03 -0700

    love working with recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -21,8 +21,8 @@ int _sqrt(int n, int i)
 if (n < 0)
 return (-1);
 if ((i * i) == n)
-return (1);
+return (i);
 if (i * i > n)
 return (-i);
-return (_sqrt(n, i + 1));
+return (_sqrt(n + 1, i));
 }
