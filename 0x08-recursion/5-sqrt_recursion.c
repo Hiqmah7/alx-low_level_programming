40526119712646b4c507fbddf98c2a6bac6753bd
Hiqmah7 <auduabdulhakeem@gmail.com>
2023-04-05 16:27:02 -0700

    recursion made easy

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,33 +1,30 @@
-
 #include "main.h"
 
 /**
- * sqrt_find - first checks for the square root of given number
- * @i: the root to check through
- * @n: number to find sqrt of
- *
- * Return: The sqrt of C or -1 if number does not have natural sqrt
- */
-
-int sqrt_find(int n, int i)
+* _sqrt_recursion - returns the natural square root of a number
+* @n: number to be used
+*
+* Return: the square root of n
+*/
+int _sqrt_recursion(int n)
 {
-if (i * i == n)
-return (i);
-if (i * i > n)
-return (-1);
-return (sqrt_find(i + 1, n));
+if (n == 0 || n == 1)
+return (n);
+return (_sqrt(0, n));
 }
 
 /**
- * _sqrt_recursion - returns the natural square root of a number
- * @n: number to find sqrt of
+ * _sqrt - returns the square root of a number
+ * @n: test number
+ * @m: squared number
  *
- * Return: natural square root or -1(if not natural square root)
+ * Return: the square root of n
  */
-
-int _sqrt_recursion(int n)
+int _sqrt(int n, int m)
 {
-if (n == 0)
-return (0);
-return (sqrt_find(1, n));
+if (n > m / 2)
+return (-1);
+else if (n * n == m)
+return (n);
+return (_sqrt((n + 1), m));
 }
