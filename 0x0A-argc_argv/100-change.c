37882c0861af779fa3f5f2fbc1eea19aa88fb96a
Hiqmah7 <auduabdulhakeem@gmail.com>
2023-04-10 08:17:55 -0700

    learning argc/argv

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
-#include<stdarg.h>
+#include <stdarg.h>
+#include "main.h"
 /**
  * main -> prints min cointo make change
  * @argc: no of args
