c25adc1cb68aa10637ee59c2f6af72d278a973aa
Hiqmah7 <auduabdulhakeem@gmail.com>
2023-04-23 18:11:46 -0700

    function-pointers

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+/**
+ * int_index - serch an for presence of int
+ * @array: array
+ * @size: size of array
+ * @cmp: pointer
+ * Return: retrurns an int
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int i;
+if (array && cmp)
+{
+for (i = 0; i < size; i++)
+{
+if (cmp(array[i]) != 0)
+return (i);
+}
+}
+return (-1);
+}
